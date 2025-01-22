07cbf9b7a3bb5e2b84d8bec2a01d2b153cf9e217
klever1995 <klever_mix@hotmail.com>
2025-01-22 14:44:35 -0600

    fix: :sparkles: "Delete unnecessary files"

--- probar.cs
+++
@@ -1,7 +0,0 @@
-using var command = connection.CreateCommand();
-command.CommandText = "SELECT * FROM Users";
-using var reader = command.ExecuteReader();
-while (reader.Read())
-{
-    Console.WriteLine($"ID: {reader["id"]}, Username: {reader["username"]}, Email: {reader["email"]}");
-}
