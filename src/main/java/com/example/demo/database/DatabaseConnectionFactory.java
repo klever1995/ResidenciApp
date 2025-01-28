5982b965a747bcff4df3a4a42c774a37cfa6c5f2
Kevin Santiago Zurita <dog.lion2298@outlook.com>
2025-01-28 17:14:34 -0500

    feat: :fire: Delete files

--- src/main/java/com/example/demo/database/DatabaseConnectionFactory.java
+++
@@ -1,8 +0,0 @@
-// Interface for creating database connection objects. Implementations will create connections for specific databases.
-
-package com.example.demo.database;
-
-public interface DatabaseConnectionFactory {
-    DatabaseConnection createConnection();
-    
-}
--- src/main/java/com/example/demo/propertyService/repository/PropertyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.propertyService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.propertyService.dto.PropertyDTO;
-
-public interface PropertyRepository extends JpaRepository<PropertyDTO, Integer> {
-}
