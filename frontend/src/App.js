44e3f38ecca2740286c0f1581a9a5b7a5b656037
klever1995 <klever_mix@hotmail.com>
2025-01-29 10:04:58 -0600

    Added PropertyAdmin.js and its CSS for property management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PropertyAdmin from './PropertyAdmin'; // Importa el componente PropertyAdmin
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bienvenido a la Administración de Propiedades</h1>
+        {/* Llamar al componente PropertyAdmin aquí */}
+        <PropertyAdmin />
       </header>
     </div>
   );
